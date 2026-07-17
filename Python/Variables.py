6c1b19832835af1a75a6ec698c269157be231544
Darrel <damadarrel12@gmail.com>
2026-07-17 10:25:00 +0100

    Variables

---
+++ Python/Variables.py
@@ -0,0 +1,18 @@
+Class = "Upper SIxth"
+print(Class)
+note = 20
+print(f"I am in {Class} and my note is {note}")
+# strings and intergers
+year = 2028
+print(f"Next year is {year}")
+
+
+num_of_students = 36
+print(f" In my class, theere are {num_of_students} students")
+average = 17.28
+print(f" My average this year is {average}")
+
+
+has_passed = True
+print(f" I have passed this year: {has_passed}")
+#boolean 
