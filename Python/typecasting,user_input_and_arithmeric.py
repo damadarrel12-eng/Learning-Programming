457307847ecc7abb5d2e81d22bf5d06a931fbe82
Darrel <damadarrel12@gmail.com>
2026-07-17 10:49:48 +0100

    Basics

---
+++ Python/typecasting,user_input_and_arithmeric.py
@@ -0,0 +1,15 @@
+name = "Deli Dama"
+age = 17
+age = str(age)
+print(f"I'm {name} and I'm {age} years old")
+
+no_of_books = input("Enter the number of books you have: ")
+no_of_books = int(no_of_books)
+print(f"You have {no_of_books} books.")
+
+name = bool(name)
+print(name)
+
+x = 15
+y =32
+print({x + y}, {x - y}, {x * y}, {x / y}, {x % y}, {x ** y})
